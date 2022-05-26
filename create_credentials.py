437f3595609481e7dedc2464ac7723d826684249
gregg-brown-sky-uk <gregg.brown@sky.uk>
2022-05-26 10:16:31 +0100

    Comments added to code

--- create_credentials.py
+++ create_credentials.py
@@ -1,9 +1,13 @@
+# Netflix Partner Report Credential Generator
+# Gregg Brown - gregg.brown@sky.uk
+
 from cryptography.fernet import Fernet
 from getpass import getpass
 from json import dumps
 from os.path import exists
 
 def CreateNetflixCredentialsFile(credFile):
+    # Get login details from user
     email = input('Please enter your email address: ')
     while True:
         passw = getpass('Please enter your password: ')
@@ -11,9 +15,11 @@ def CreateNetflixCredentialsFile(credFile):
             print("Passwords don't match - try again")
         else:
             break
-
+    
+    # Create dict to hold details
     credentials = {'Netflix':{'email':f'{email}','password':f'{passw}'}}
 
+    # Create encryption keyfile, if one doesn't already exist
     fk = 'filekey.key'
     if exists(fk):
         with open(fk, 'rb') as keyFile:
@@ -24,10 +30,12 @@ def CreateNetflixCredentialsFile(credFile):
             keyFile.write(key)
     fernet = Fernet(key)
 
+    # Encrypt and write login credentials to file
     with open(credFile, 'wb') as outFile:
         outFile.write(fernet.encrypt(dumps(credentials).encode('utf-8')))
 
 if __name__ == '__main__':
+    # Only seen if script is run directly, to create or overwrite credentials file
     credFile = 'credentials.enc'
     title = '+ NetFlix Credentials File Creator +'
     decoration = '+'*len(title)
--- netflix_report_download.py
+++ netflix_report_download.py
@@ -1,3 +1,6 @@
+# Netflix Partner Report Auto Downloader
+# Gregg Brown - gregg.brown@sky.uk
+
 from selenium.webdriver import Firefox, FirefoxOptions
 from selenium.webdriver.common.by import By
 from pathlib import Path
@@ -7,18 +10,21 @@ from cryptography.fernet import Fernet
 from create_credentials import CreateNetflixCredentialsFile
 import shutil, os, json, pandas as pd
 
+# Intro text
 title = '+ NetFlix Partner Report Downloader +'
 decoration = '+'*len(title)
 print(f'{decoration}\n{title}\n{decoration}\n')
 
 print('Process started...')
+# Set necessary variables
 thisMonth = dt.strftime(date.today(), '%B %Y')
-url = f'https://partner-reports.netflixpartners.com/' # Netflix login
+url = f'https://partner-reports.netflixpartners.com/'
 saveDir = f'{Path.home()}\Downloads\{thisMonth} Netflix'
 fileMonth = dt.strftime(date.today().replace(day=1) - td(days=1), '%b_%y')
 fileNames = [fr'{saveDir}\Netflix_Bundle_Invoice_Report_Data_Sky_UK_{region}_{fileMonth}.csv.zip' 
                 for region in ['GB','IE']]
 
+# Get login details from credentials file, if does not exists calls function from create_credentials script
 credFile = 'credentials.enc'
 while True:
     if os.path.exists(credFile):
@@ -30,6 +36,7 @@ while True:
     else:
         CreateNetflixCredentialsFile(credFile)
 
+# List of HTML elements required for navigating login pages
 xpaths = {
     'username':
         r'/html/body/div[2]/div/div/div[2]/form/div[1]/input',
@@ -43,23 +50,28 @@ xpaths = {
         r'/html/body/div[2]/div[1]/div[2]/div[4]/a/div/p[1]'
 }
 
+# Create browser object and set options to automatically save zip files without prompt
 options = FirefoxOptions()
 options.set_preference('browser.download.folderList', 2)
 options.set_preference('browser.download.dir', saveDir)
 options.set_preference('browser.download.manager.showWhenStarting', False)
 options.set_preference('browser.helperApps.neverAsk.saveToDisk', 'application/zip')
-options.add_argument('--headless')
+options.add_argument('--headless') # Run browser in background, comment this line to make visible
 
+# Use browser
 with Firefox(options=options) as browser:
-    browser.get(url=url)
+    browser.get(url=url) # Go to login
     print('Logging onto Netflix Partner Site..', end='\r')
-    browser.implicitly_wait(30)
+    browser.implicitly_wait(30) # Allow extra time for elements to appear on page
+    # Login
     browser.find_element(By.XPATH, xpaths['username']).send_keys(credentials['email'])
     browser.find_element(By.XPATH, xpaths['continue']).click()
     browser.find_element(By.XPATH, xpaths['password']).send_keys(credentials['password'])
     browser.find_element(By.XPATH, xpaths['login']).click()
+    # Search page for current month's data
     monthSection = browser.find_element(By.XPATH, f"//*[contains(text(), '{thisMonth}')]")
     print('Downloading zip files...', end='\r')
+    # Download both zip files and wait until both csv have been extracted from zip before closing browser
     for i in range(1,3):
         monthSection.find_element(By.XPATH, f'../section[{i}]/div[2]/div/button').click()
     while True:
@@ -72,12 +84,14 @@ with Firefox(options=options) as browser:
         except:
             sleep(2)
 
+# More variables
 csvFiles = [f'{saveDir}\{csvFile}' for csvFile in os.listdir(saveDir)]
 dateCols = ['Report_Month', 'Event_Date', 'Cancel_Date']
 gcpSchema = [{'name':col,'type':'date'} for col in dateCols]
 projectId='skyuk-uk-csgbillanalysis-dev'
 destTable=f'Sandpit.GB_Netflix_Invoice_{fileMonth}'
 
+# Extract, transform and upload data from csv files to GCP
 for item, csvFile in enumerate(csvFiles):
     print(f'Loading data from {csvFile}...', end='\r')
     df = pd.read_csv(csvFile)
@@ -91,3 +105,4 @@ for item, csvFile in enumerate(csvFiles):
                 location='EU', table_schema=gcpSchema)
 
 print('Process Ended')
+# End of processs
